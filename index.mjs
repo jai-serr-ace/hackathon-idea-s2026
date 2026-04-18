7c15506b28547131ab84a31de94e76dc1b11021b
Adrik <adrikrenteria@gmail.com>
2026-04-18 16:32:36 -0700

    initalized the node package

--- index.mjs
+++ index.mjs
@@ -1,1 +1,15 @@
-console.log("hackathon-idea-s2026 is initialized");
+import express from 'express';
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+
+app.get('/', (req, res) => {
+   res.send('Hello Express app!')
+});
+
+
+app.listen(3000, () => {
+   console.log('server started');
+});
+
